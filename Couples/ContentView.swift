806b301ac6b133783a571203b4552452f3082095
monhtulga <munkhtulga.es@gmail.com>
2025-04-22 02:11:42 +0800

    Initial Commit

---
+++ Couples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Couples
+//
+//  Created by monhtulga elbegsaihan on 4/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Couples/CouplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouplesApp.swift
+//  Couples
+//
+//  Created by monhtulga elbegsaihan on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CouplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
